5734bff1c8e3efd7e812098fff69ed6d6b59d3ac
GRy82 <gwryan@buffalo.edu>
2021-02-16 10:43:00 -0500

    Code cleaned up/organized, not refactored.

--- Main.java
+++ Main.java
@@ -5,21 +5,23 @@ public class Main{
     public static void main(String[] args){
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Principal loan amount: $");
         double principal = scanner.nextDouble();
-
         System.out.print("Annual interest rate: ");
         float annualInterestRate = scanner.nextFloat();
         System.out.print("Payment period(years): ");
-        int period = scanner.nextInt();
+        byte period = scanner.nextByte();
         scanner.close();
 
         double numberPayments = period * 12;
         double monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
 
-        double mortgage = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberPayments) / 
-                            (Math.pow(1 + monthlyInterestRate, numberPayments) - 1));
+        double mortgage = principal
+                            * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberPayments)  
+                            / (Math.pow(1 + monthlyInterestRate, numberPayments) - 1));
+
         String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.print("Mortgage payment: " + formattedMortgage + "\n");
 
