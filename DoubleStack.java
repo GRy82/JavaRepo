b2e6307ad87091877c24a404ceaa15bad1958687
GRy82 <gwryan@buffalo.edu>
2021-02-22 10:32:48 -0500

    Push1 and Push2 methods implemented. push1 loads at the front of the array. push2 loads at the tail.

--- DoubleStack.java
+++ DoubleStack.java
@@ -1,14 +1,18 @@
 public class DoubleStack {
     private int[] stack = new int[10];
     private int limiter1;
-    private int limiter2;
+    private int limiter2 = stack.length - 1;
 
-    public void push1(){
-        
+    public void push1(int pushed){
+        if (isFull1()) throw new StackOverflowError();
+
+        stack[limiter1++] = pushed;
     }
 
-    public void push2(){
+    public void push2(int pushed){
+        if (isFull2()) throw new StackOverflowError();
 
+        stack[limiter2--] = pushed;
     }
 
     public int pop1(){
