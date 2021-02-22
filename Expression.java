cb0f355deb2f9d30df9072fdefad1989014690ee
GRy82 <gwryan@buffalo.edu>
2021-02-21 19:49:16 -0500

    isBalanced method refactored

--- Expression.java
+++ Expression.java
@@ -1,32 +1,39 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Expression {
+
+    private final List<Character> leftBrackets = Arrays.asList('(', '[', '{', '<');
+    private final List<Character> rightBrackets = Arrays.asList(')', ']', '}', '>');
+
     public boolean isBalanced(String input){
         Stack<Character> brackets = new Stack<>();
 
         for (char character : input.toCharArray()){
-            if(character == '(' || character == '<' || character == '['){
+            if(isLeftBracket(character)){
                 brackets.push(character);
             }
 
-            if(character == ')' || character == '>' || character == ']'){
+            if(isRightBracket(character)){
                 if(brackets.empty()) return false;
-                char stackBracket = brackets.peek();
-                if((character == '>' || character == ']') && stackBracket != character + 2)
-                    return false;
-                else if(character == ')' && stackBracket != character + 1){
-                    return false;
-                }
-                else{
-                    brackets.pop();
-                }
+
+                if(!bracketsMatch(character, brackets.pop())) return false;
             }
         }
 
-        if(brackets.empty())
-            return true;
-        else
-            return false;
-        
+        return brackets.empty();   
+    }
+
+    private boolean isLeftBracket(char character){
+        return leftBrackets.contains(character);
+    }
+
+    private boolean isRightBracket(char character){
+        return rightBrackets.contains(character);
+    }
+
+    private boolean bracketsMatch(char character, char popped){
+        return leftBrackets.indexOf(popped) == rightBrackets.indexOf(character);
     }
 }
