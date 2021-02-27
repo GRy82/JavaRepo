a8df4121f077cf80ac30fa7910f962804b12a767
GRy82 <gwryan@buffalo.edu>
2021-02-27 11:57:28 -0500

    findFirstNonRepeating simplified using a ternary. also Character.MIN_VALUE employed for when there are no non-repeated characters.

--- FirstNonRepeating.java
+++ FirstNonRepeating.java
@@ -2,22 +2,20 @@ import java.util.HashMap;
 
 public class FirstNonRepeating {
     public static char findFirstNonRepeating(String phrase){
+        
         HashMap<Character, Integer> repeats = new HashMap<>();
-        for(char letter : phrase.toCharArray())
-            if (!repeats.containsKey(letter))
-                repeats.put(letter, 1);
-            else{
-                var value = repeats.get(letter);
-                repeats.replace(letter, ++value);
-            }
-        char firstNonRepeat = ' ';
-        for (var pair : repeats.entrySet()){
-            if (pair.getValue() == 1){
-                firstNonRepeat = pair.getKey();
-                break;
-            }
-        }
 
-        return firstNonRepeat;
+        char[] chars = phrase.toCharArray();
+
+        for(var letter : chars){
+            var value = repeats.containsKey(letter) ? repeats.get(letter) + 1 : 1;
+            repeats.put(letter, value);
+        }
+           
+        for(var letter : chars)
+            if (repeats.get(letter) == 1)
+                return letter;
+
+        return Character.MIN_VALUE;
     }
 }
